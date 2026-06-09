1c21f9d4d9c3ab0ad4165d8fe2c346ded89b2abf
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-06-09 15:38:28 +0900

    [추가] 비즈니스 예외 클래스 생성

---
+++ Backend/src/main/java/com/shuttleplay/server/global/error/BusinessException.java
@@ -0,0 +1,18 @@
+package com.shuttleplay.server.global.error;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(ErrorCode errorCode, String detail) {
+        super(detail);
+        this.errorCode = errorCode;
+    }
+}
