159722bf6d0e048552245e929e09e905ec05e157
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-06-10 02:41:22 +0900

    [수정] Refresh Token 엔티티에 기본키 필드 추가

--- Backend/src/main/java/com/shuttleplay/server/domain/auth/entity/RefreshToken.java
+++ Backend/src/main/java/com/shuttleplay/server/domain/auth/entity/RefreshToken.java
@@ -3,6 +3,9 @@ package com.shuttleplay.server.domain.auth.entity;
 import com.shuttleplay.server.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -14,6 +17,10 @@ import lombok.NoArgsConstructor;
 @Table(name = "refresh_tokens")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RefreshToken extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Column(nullable = false)
     private Long userId;
 
